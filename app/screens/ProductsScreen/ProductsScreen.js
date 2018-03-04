d6ea4ddbedac910e1d8872f894176dd273f25c7a
Unknown <damiano.calcagni@gmail.com>
2018-03-04 23:35:52 +0100

    Improves ProductsScreen
    - Adds custom function to update items in the view
    - Implements hiding and showing for products by category
    - Adds new icon

--- app/screens/ProductsScreen/ProductsScreen.js
+++ app/screens/ProductsScreen/ProductsScreen.js
@@ -18,27 +18,85 @@ export default class ProductsScreen extends Component {
         this.state = {
             sections: []
         };
-        settings.getProductsList().then(function(result) {
-            var sections = [];
-            result.forEach(product => {
-                var isFound = false;
-                product['quantity'] = 0;
 
-                for (i = 0; i < sections.length; i++) {
-                    if (sections[i]['title'] === product.cat) {
-                        isFound = true;
-                        sections[i]['data'].push(product);
-                    }
-                }
-                
-                if (!isFound) {
-                    sections.push({'title': product.cat, 'data': [ product ]});
-                }
-            });
-            this.setState({'sections': sections});
+        settings.getCategoriesList().then(function(result) {
+            this.updateCategoriesList(result);
         }.bind(this), function(reason) {
             console.error(reason);
         });
+
+        settings.getProductsList().then(function(result) {
+            this.updateProductsList(result);
+            settings.getNewsList().then(function(result) {
+                this.updateNewsList(result);
+            }.bind(this), function(reason) {
+                console.error(reason);
+            });
+        }.bind(this), function(reason) {
+            console.error(reason);
+        });
+    }
+
+    updateCategoriesList(categories) {
+        var sections = this.state.sections;
+        categories.forEach(category => {
+            var isFound = false;
+            sections.forEach(section => {
+                if (section.title === category.cat) {
+                    isFound = true;
+                }
+            });
+
+            if (!isFound) {
+                sections.push({title: category.cat, active: true, data: []});
+            }
+        });
+        this.setState({'sections': sections});
+    }
+
+    updateProductsList(products) {
+        var sections = this.state.sections;
+        products.forEach(product => {
+            var isFound = false;
+            product.quantity = 0;
+            product.isNew = false;
+
+            sections.forEach(section => {
+                if (section.title === product.cat) {
+                    isFound = true;
+                    section.data.push(product);
+                }
+            });
+            
+            if (!isFound) {
+                sections.push({title: product.cat, active: true, data: [ product ]});
+            }
+        });
+        this.setState({'sections': sections});
+    }
+
+    updateNewsList(products) {
+        var sections = this.state.sections;
+        products.forEach(product => {
+            sections.forEach(section => {
+                section.data.forEach(item => {
+                    if (item.idProd === product.idProd) {
+                        item.isNew = true;
+                    }
+                });
+            });
+        });
+        this.setState({'sections': sections});
+    }
+
+    isItemActive(item) {
+        var result = true;
+        this.state.sections.forEach(section => {
+            if (section.title === item.cat) {
+                result = section.active;
+            }
+        });
+        return result;
     }
 
     render() {
@@ -48,9 +106,12 @@ export default class ProductsScreen extends Component {
                 sections={this.state.sections}
                 renderItem= {
                     ({item}) =>
-                    <View style= {styles.itemContainer}>
+                    <View style={this.isItemActive(item) ? styles.itemContainerActive : styles.itemContainerInactive}>
                         <View style= {{ flex: 1, flexDirection: 'row', justifyContent: 'flex-start' }}>
                             <Image source= {{ uri: item.imgUri }} style= {{ width: 50, height: 50 }} />
+                            <Image 
+                            source = {require('../../../assets/new-256.png')} 
+                            style = {item.isNew ? { position: 'absolute', top: - 5, left: - 10, width: 20, height: 20 } : { display: 'none' }} />
                             <Text style= {styles.item}> {item.descr} </Text>
                         </View>
                         <View style= {{ flex: 2, flexDirection: 'row', justifyContent: 'center' }}>
@@ -63,7 +124,14 @@ export default class ProductsScreen extends Component {
                         </View>
                     </View>
                 }
-                renderSectionHeader={({section}) => <Text style= {styles.sectionHeader}> {section.title} </Text>}
+                renderSectionHeader={({section}) => 
+                    <Button 
+                    style={styles.sectionHeader} 
+                    title={section.title} 
+                    titleStyle={styles.titleStyle} 
+                    onPress={() => { section.active = !section.active; this.setState(this.state); }}
+                    />
+                }
                 keyExtractor={(item, index) => index}
             />
         </View>
