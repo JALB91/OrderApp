9e51c9dc4b45440c48658b5745723824ae6d34ce
Unknown <damiano.calcagni@gmail.com>
2018-03-04 23:31:46 +0100

    Adds styles.js file for HomeScreen

--- app/screens/HomeScreen/HomeScreen.js
+++ app/screens/HomeScreen/HomeScreen.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import {
   Platform,
-  StyleSheet,
   Text,
   View
 } from 'react-native';
 import { StackNavigator } from 'react-navigation';
-import Button from '../../components/Button/Button'
+import Button from '../../components/Button/Button';
+import styles from './styles';
 
 export default class HomeScreen extends Component {
     constructor(props) {
@@ -15,30 +15,21 @@ export default class HomeScreen extends Component {
   
     render() {
       return (
-        <View style= {{ flex: 2, alignItems: 'center', padding: 10 }}>
-            <Text style= {{ fontSize: 30, margin: 50 }}>Home Screen</Text>
+        <View style = {{ flex: 2, alignItems: 'center', padding: 10 }}>
+            <Text style = {{ fontSize: 30, margin: 50 }}>Home Screen</Text>
             <Button
-            style={styles.button}
-            onPress={() => this.props.navigation.navigate('Products')} 
-            title={'Go to Products'} 
-            titleStyle={{ fontSize: 18 }}
+            style = {styles.button}
+            onPress = {() => this.props.navigation.navigate('Products')} 
+            title = {'Go to Products'} 
+            titleStyle = {{ fontSize: 18 }}
             />
             <Button 
-            style={styles.button}
-            onPress={() => this.props.navigation.navigate('Orders')}
-            title={'Go to Orders'}
-            titleStyle={{ fontSize: 18 }}
+            style = {styles.button}
+            onPress = {() => this.props.navigation.navigate('Orders')}
+            title = {'Go to Orders'}
+            titleStyle = {{ fontSize: 18 }}
             />
         </View>    
       );
     }
 }
-
-const styles = StyleSheet.create({
-  button: {
-    margin: 10,
-    width: 150,
-    backgroundColor: 'rgba(100,200,200,1.0)',
-    alignItems: 'center'
-  }
-});
